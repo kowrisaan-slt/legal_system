3b9eb9f22425a32a2e906c3f4d79ea3ded609d5e
kowrisaan-slt <kowrisaan@slt.com.lk>
2026-03-19 16:58:10 +0530

    Update Login.tsx

--- Legal_Management_System/client/src/pages/Login.tsx
+++ Legal_Management_System/client/src/pages/Login.tsx
@@ -13,7 +13,7 @@ const Login: React.FC = () => {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         try {
-            const API_URL = import.meta.env.VITE_API_URL;
+            const res = await axios.post('http://legal-system.apps.openshift-01.akaza.lk:5000/auth/login', { email, password });
             login(res.data.token, res.data.user);
             navigate('/modules');
         } catch (err: any) {
